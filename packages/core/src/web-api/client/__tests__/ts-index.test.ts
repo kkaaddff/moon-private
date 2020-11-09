dccccd0a7260f1dad58380d3130c88ea5a880bf1
yangxiaodong <yangxiaodong@qianmi.com>
2020-11-09 20:19:07 +0800

    add: 添加url地址

--- packages/core/src/web-api/client/__tests__/ts-index.test.ts
+++ packages/core/src/web-api/client/__tests__/ts-index.test.ts
@@ -13,23 +13,20 @@ import {join} from  'path';
 
 
 
-console.log(1111111111);
-let tt  =  genApiTsIndex(
-  {
-    tsConfig:join(__dirname,"./datas/api/tsconfig.json"),
-    apiDir:join(__dirname,"./datas/api"),
-    // filter:({filePath})=>{
-    //   return filePath.includes("asset-change-controller");
-    // }
-  }
-);
 
-console.log(tt);
-//
-// describe('api索引', () => {
-//   it('basic', async () => {
-//     console.log(111)
-//
-//     expect('hello').toEqual("hello");
-//   });
-// });
+describe('api索引', () => {
+  it('basic', async () => {
+
+    let tt  =  genApiTsIndex(
+      {
+        tsConfig:join(__dirname,"./datas/api/tsconfig.json"),
+        apiDir:join(__dirname,"./datas/api"),
+        // filter:({filePath})=>{
+        //   return filePath.includes("asset-change-controller");
+        // }
+      }
+    );
+
+    expect(tt).toMatchSnapshot();
+  });
+});
--- packages/core/src/web-api/client/ts-index.ts
+++ packages/core/src/web-api/client/ts-index.ts
@@ -7,39 +7,45 @@
  *
  * @Date    2019/5/20
  **/
-import {Project, InterfaceDeclarationStructure, StructureKind, TypeAliasDeclarationStructure} from 'ts-morph';
+import {
+  Project,
+  InterfaceDeclarationStructure,
+  StructureKind,
+  TypeAliasDeclarationStructure,
+} from 'ts-morph';
 
 import debug from 'debug';
-const baseType = ['number', 'string','unknown','boolean'];
+const baseType = ['number', 'string', 'unknown', 'boolean'];
 const numberReg = /^[0-9]+$/;
 let log = debug('web-api:ts-index');
-export interface Controller{
-  fileName:"",
-  methods:{
-    [methodName:string]:{
-      responseTs:string[];
-    }
-  }
+export interface Controller {
+  fileName: '';
+  methods: {
+    [methodName: string]: {
+      responseTs: string[];
+    };
+  };
 }
 
+export interface ApiIndex {}
 
-export interface ApiIndex {
-
-
-}
+const urlReg= new RegExp("`(.*)`");
 
 export function genApiTsIndex({
-                                tsConfig,apiDir,apiSuffix,filter}:{
-  apiDir:string;
-  tsConfig:string;
-  apiSuffix?:string;
-  filter?:(param:{filePath:string})=>boolean;
+  tsConfig,
+  apiDir,
+  apiSuffix,
+  filter,
+}: {
+  apiDir: string;
+  tsConfig: string;
+  apiSuffix?: string;
+  filter?: (param: {filePath: string}) => boolean;
 }) {
-
   const project = new Project({
-    tsConfigFilePath:tsConfig,
+    tsConfigFilePath: tsConfig,
   });
-  let result = project.addExistingSourceFiles(apiDir+"**/*.ts");
+  let result = project.addExistingSourceFiles(apiDir + '**/*.ts');
 
   let apiTeIndex = {};
 
@@ -51,13 +57,13 @@ export function genApiTsIndex({
     let fileController = result[fileIndex];
     let fileName = fileController.getBaseNameWithoutExtension();
 
-    if(apiSuffix) {
-      if(!fileName.endsWith(apiSuffix)) {
+    if (apiSuffix) {
+      if (!fileName.endsWith(apiSuffix)) {
         continue;
       }
     }
 
-    if(filter && !filter({filePath:fileName})) {
+    if (filter && !filter({filePath: fileName})) {
       continue;
     }
 
@@ -70,37 +76,52 @@ export function genApiTsIndex({
     let allInterface = fileController
       .getStructure()
       // @ts-ignore
-      .statements.filter(item => [StructureKind.Interface,StructureKind.TypeAlias].includes(item.kind) );
+      .statements.filter((item) =>
+        [StructureKind.Interface, StructureKind.TypeAlias].includes(item.kind),
+      );
     //获取所有function方法.
+    let controllerFuns =fileController.getFunctions();
     for (
-      let i = 0, iLen = fileController.getStructure().statements.length;
+      let i = 0, iLen = controllerFuns.length;
       i < iLen;
       i++
     ) {
-      let statement = fileController.getStructure().statements[i];
+      let functionDec =controllerFuns[i];
+
+      let statement = functionDec.getStructure();
 
       if (statement.kind === StructureKind.Function) {
+        let [_,url] = functionDec.getBodyText().match(urlReg);
 
         let responseTs = statement.returnType
           ? getAllTsNameRef(
+              allInterface,
             //@ts-ignore
-            allInterface,
-            statement.returnType.replace('Promise<', '').replace('>', ''),
-          )
+              statement.returnType.replace('Promise<', '').replace('>', ''),
+            )
           : [];
+
         allMethods[statement.name] = {
+          methdoName:statement.name,
           responseTs,
-          params:statement.parameters.map(item=>{
+          url,
+          params: statement.parameters.map((item) => {
             // console.log(item.type);
             //@ts-ignore
-            let results = (item.type as string).matchAll(/([a-zA-Z0-9_]*): ([a-zA-Z0-9_]*);/);
-            // console.log(Array.from(results).map(item=>item));
-            // console.log('---------')
+            let results = (item.type as string).matchAll(
+              /([a-zA-Z0-9_]*) ?\?? ?: ?([a-zA-Z0-9_]*);/,
+            );
+
             return {
-              name:item.name,
-              subTypes:Array.from(results).map(item=>({name:item[1],type:item[2]})),
-            }
-          })
+              name: item.name,
+
+              subTypes: Array.from(results).map((item) => ({
+                name: item[1],
+                isRequired:item[0].includes('?'),
+                type: item[2],
+              })),
+            };
+          }),
         };
       }
     }
@@ -108,7 +129,6 @@ export function genApiTsIndex({
 
   return apiTeIndex;
 }
-
 
 /**
  * 获取ts定义的事情 ...
@@ -119,43 +139,45 @@ export function genApiTsIndex({
  * @returns {string[]}
  */
 function getAllTsNameRef(
-  interfaces: (InterfaceDeclarationStructure |TypeAliasDeclarationStructure)[],
+  interfaces: (InterfaceDeclarationStructure | TypeAliasDeclarationStructure)[],
   name: string,
   results: string[] = [],
 ): string[] {
   if (name.endsWith('[]') && !results.includes(name)) {
     name = name.replace('[]', '');
-    if(isRefTs(name) ) {
-      results.push(name+"[]");
+    if (isRefTs(name)) {
+      results.push(name + '[]');
     }
   }
-
-
 
   for (let i = 0, iLen = interfaces.length; i < iLen; i++) {
     let interfaceItem = interfaces[i];
     if (interfaceItem.name === name) {
       //如果是type则继续传递向下找;
-      if( StructureKind.TypeAlias === interfaceItem.kind){
-        if(typeof interfaceItem.type ==='string'){
+      if (StructureKind.TypeAlias === interfaceItem.kind) {
+        if (typeof interfaceItem.type === 'string') {
           results.push(interfaceItem.name);
-          getAllTsNameRef(interfaces,interfaceItem.type,results);
+          getAllTsNameRef(interfaces, interfaceItem.type, results);
         }
       }
 
-      if( StructureKind.Interface === interfaceItem.kind){
+      if (StructureKind.Interface === interfaceItem.kind) {
         // @ts-ignore
         // console.log(interfaces.properties);
         //遍历获取子依赖..
         results.push(name);
         if (interfaceItem.properties) {
           // @ts-ignore
-          for (let j = 0, jLen = interfaceItem.properties.length; j < jLen; j++) {
+          for (
+            let j = 0, jLen = interfaceItem.properties.length;
+            j < jLen;
+            j++
+          ) {
             // @ts-ignore
             let property = interfaceItem.properties[j];
             //基本类型不统计在内了.,只统计interface与type类型的.
             //@ts-ignore
-            if(['number','string','YearMonth'].includes(property.type)) {
+            if (['number', 'string', 'YearMonth'].includes(property.type)) {
               continue;
             }
 
@@ -170,15 +192,8 @@ function getAllTsNameRef(
                 }
               }
             } else {
-              if (
-                isRefTs(_propertyType) &&
-                !results.includes(_propertyType)
-              ) {
-                results = getAllTsNameRef(
-                  interfaces,
-                  _propertyType,
-                  results,
-                );
+              if (isRefTs(_propertyType) && !results.includes(_propertyType)) {
+                results = getAllTsNameRef(interfaces, _propertyType, results);
               }
             }
           }
@@ -205,6 +220,12 @@ function isRefTs(refInfo: string): boolean {
   } else if (numberReg.test(refInfo.trim())) {
     return false;
   }
-  log(`判断是否是引用类型;${numberReg.test('1')} -${refInfo}- VS -${refInfo.trim()}-` ,numberReg.test(refInfo+""), typeof refInfo);
+  log(
+    `判断是否是引用类型;${numberReg.test(
+      '1',
+    )} -${refInfo}- VS -${refInfo.trim()}-`,
+    numberReg.test(refInfo + ''),
+    typeof refInfo,
+  );
   return true;
 }
