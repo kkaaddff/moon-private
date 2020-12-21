a7e85e6f4d8cae6c6294c6637700557abfd7d07a
zhangqicheng <zhangqicheng@yunzhangfang.com>
2020-12-21 23:45:37 +0800

    🐞fix(删除pages):

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,8 +7,7 @@
  *
  **/
 
-import * as ReduxGen  from  './page/redux/redux';
-import * as JsonUtil from  './util/json-util';
+import * as JsonUtil from './util/json-util';
 import * as StringUtil from './util/string-util';
 import * as JestUtil from './util/jest-util';
 import * as CompileUtil from './util/compile-util';
@@ -16,17 +15,16 @@ import * as WebApiGen from './web-api/client';
 import * as SwaggerUtil from './web-api/client/util/swagger';
 import * as fakeGen from './web-api/client/fake-gen';
 import * as TsIndex from './web-api/client/ts-index';
-import * as apiDomain from  './web-api/client/domain';
+import * as apiDomain from './web-api/client/domain';
 
 export const api = {
-  domain:apiDomain
+  domain: apiDomain
 }
 
 export default {
   // api:{
   //   domain:apiDomain
   // },
-  ReduxGen,
   JsonUtil,
   StringUtil,
   CompileUtil,
