6c6f523d78cb51fea880b5c08cfb2721a763a42f
zhangqicheng <zhangqicheng@yunzhangfang.com>
2020-12-21 23:40:07 +0800

    🦄refactor(common pages):

--- packages/moon-common/src/copy.ts
+++ packages/moon-common/src/copy.ts
@@ -3,9 +3,4 @@ import { join } from "path";
 
 (async () => {
   let libDir = join(__dirname, "../lib");
-  // let sourceDir=join(__dirname);
-  // let targetDir =join(__dirname,"../lib/page/redux/tpl");
-
-  // fse.ensureDirSync(targetDir);
-  // fse.copySync(join(sourceDir,"db/mysql/gene/tpl"),join(libDir,"db/mysql/gene/tpl"));
 })();
