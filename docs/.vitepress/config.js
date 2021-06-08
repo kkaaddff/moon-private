879ba03af458aa4fc576b678615c3f2f38e09a17
Qic@Mac <zqc007@gmail.com>
2021-06-08 18:31:13 +0800

    docs(vitepress): 增加文档

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
+
+  themeConfig: {
+    repo: 'kkaaddff/moon-private',
+    docsDir: 'docs',
+
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+
+    nav: [
+      { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
+      {
+        text: 'Config Reference',
+        link: '/config/basics',
+        activeMatch: '^/config/',
+      },
+      {
+        text: 'Release Notes',
+        link: 'https://github.com/vuejs/vitepress/releases',
+      },
+    ],
+
+    sidebar: {
+      '/guide/': getGuideSidebar(),
+      '/config/': getConfigSidebar(),
+      '/': getGuideSidebar(),
+    },
+  },
+}
+
+function getGuideSidebar() {
+  return [
+    {
+      text: 'Introduction',
+      children: [
+        { text: 'What is VitePress?', link: '/' },
+        { text: 'Getting Started', link: '/guide/getting-started' },
+        { text: 'Configuration', link: '/guide/configuration' },
+        { text: 'Asset Handling', link: '/guide/assets' },
+        { text: 'Markdown Extensions', link: '/guide/markdown' },
+        { text: 'Using Vue in Markdown', link: '/guide/using-vue' },
+        { text: 'Deploying', link: '/guide/deploy' },
+      ],
+    },
+    {
+      text: 'Advanced',
+      children: [
+        { text: 'Frontmatter', link: '/guide/frontmatter' },
+        { text: 'Global Computed', link: '/guide/global-computed' },
+        { text: 'Global Component', link: '/guide/global-component' },
+        { text: 'Customization', link: '/guide/customization' },
+        {
+          text: 'Differences from Vuepress',
+          link: '/guide/differences-from-vuepress',
+        },
+      ],
+    },
+  ]
+}
+
+function getConfigSidebar() {
+  return [
+    {
+      text: 'App Config',
+      children: [{ text: 'Basics', link: '/config/basics' }],
+    },
+    {
+      text: 'Theme Config',
+      children: [
+        { text: 'Homepage', link: '/config/homepage' },
+        { text: 'Algolia Search', link: '/config/algolia-search' },
+        { text: 'Carbon Ads', link: '/config/carbon-ads' },
+      ],
+    },
+  ]
+}
