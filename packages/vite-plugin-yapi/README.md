92ccf00771633a2909cab16a8afe792009ab3d65
章启成 <910021538@qq.com>
2021-12-07 10:38:15 +0800

    feat(plugin-yapi): 删除

--- packages/vite-plugin-yapi/README.md
+++
@@ -1,7 +0,0 @@
-# moon 插件
-
-> 将 moon 默认导出的 ts 文件编译成 js ➕ .d.ts
-
-### 使用 rollup 打包工具
-
-项目目录必须有可用的 tsconfig.json 配置文件
--- packages/vite-plugin-yapi/__tests__/transfromts2js.spec.ts
+++
@@ -1,9 +0,0 @@
-import { isEqual } from 'lodash'
-
-describe('transfrom-plugin/transfromJson', () => {
-  it('Json After transformed should be equal to target ', () => {
-    const transfromedCtx = {}
-    // transfromJson(transfromedCtx)
-    // expect(transfromedCtx.swaggerJson).toEqual(TargetJson)
-  })
-})
