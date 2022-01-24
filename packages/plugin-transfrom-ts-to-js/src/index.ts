8f2b2080b87ed00145e95823496c4d7beb2fd9b0
章启成 <910021538@qq.com>
2022-01-24 18:57:47 +0800

    fix(buildTs): 处理index.ts

--- packages/plugin-transfrom-ts-to-js/src/index.ts
+++ packages/plugin-transfrom-ts-to-js/src/index.ts
@@ -2,7 +2,6 @@ import buildTs from './tsbuild'
 const pluginName = 'TransfromTs2JsPlugin'
 import ApiCompileHooks from '@zhangqc/moon-common/declarations/swagger-api/hook'
 import * as fse from 'fs-extra'
-import * as path from 'path'
 
 export class TransfromTsToJsPlugin {
   apply(compilerHook: ApiCompileHooks) {
@@ -16,9 +15,13 @@ export class TransfromTsToJsPlugin {
      * 将 目标的 ts 转换成 js 并且同时输出 d.ts
      */
     compilerHook.afterIndex.tap(pluginName, (options, moonContext) => {
-      debugger
-      //
-      // buildTs({ content: options, toSaveFilePath: options })
+      const indexOptions = {
+        content: fse.readFileSync(options, { encoding: 'utf8' }),
+        toSaveFilePath: options,
+      }
+      buildTs(indexOptions)
+      fse.removeSync(options)
+      fse.writeFileSync(indexOptions.toSaveFilePath.replace(/\.ts$/, '.js'), indexOptions.content)
     })
   }
 }
